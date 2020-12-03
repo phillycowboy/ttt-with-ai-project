420a12746afa31ddd73ad9086c9da840e0572604
Ben Harter-Murphy <benhartermurphy@gmail.com>
2020-12-03 14:35:32 -0500

    commit tree f8aceee05a15cf8add6c4b114fc60fd8e57a4910

---
+++ lib/players/computer.rb
@@ -0,0 +1,13 @@
+module Players
+
+    class Computer < Player
+        def move(board)
+            move = nil
+            if !board.taken?(5)
+                move = "5"
+            end
+
+        end
+    end
+
+end 
